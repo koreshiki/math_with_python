e69e8e7ec61eab8f9e80007cd723650586434e5e
koreshiki <shooting.ray41@gmail.com>
2020-05-20 22:23:43 +0900

    complete creating group_frequcency.py

--- math_with_python/chap3/group_frequency.py
+++ math_with_python/chap3/group_frequency.py
@@ -1,3 +1,6 @@
+import math
+
+
 def create_classes(numbers, n):
     low = min(numbers)
     high = max(numbers)
@@ -16,15 +19,28 @@ def create_classes(numbers, n):
 
 def calc_group_frequency(numbers, classes):
     num_groups = len(classes)
+    N = len(numbers)
     numbers.sort()
+    print(numbers)
     frequency = []
-    for num in numbers:
-        for c in classes:
-            if num < c[1]:
-                print('a')
+    idx = 0
+    for i in range(num_groups):
+        count = 0
+        while True:
+            if idx < N and numbers[idx] > classes[i][1] or idx == N:
+                break
+            idx += 1
+            count += 1
+        frequency.append(count)
+
+    return frequency
 
 
 if __name__ == '__main__':
     numbers = [7, 8, 9, 2, 10, 9, 9, 9, 9, 4, 5, 6, 1, 5, 6, 7, 8, 6, 1, 10]
-    classes = create_classes(numbers, 2)
-    calc_group_frequency(numbers, classes)
+    classes = create_classes(numbers, 4)
+    print(classes)
+    freq = calc_group_frequency(numbers, classes)
+
+    for i in range(len(freq)):
+        print(f'{math.ceil(classes[i][0])}\t-\t{math.ceil(classes[i][1])}\t:\t{freq[i]}')
