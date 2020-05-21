019ad8278d58274fee4cdd0bdbeb0c7e6816213d
koreshiki <shooting.ray41@gmail.com>
2020-05-21 19:54:56 +0900

    first commit

---
+++ math_with_python/chap4/print_series.py
@@ -0,0 +1,23 @@
+"""
+Print the series
+    x^2   x^3          x^n
+x + ---- + ---- + ... + ----
+     2      3            n
+
+and calculate its value at a certain value of x
+"""
+from sympy import Symbol, pprint, init_printing
+
+
+def print_series(n):
+    # initialize printing system with reverse order
+    x = Symbol('x')
+    series = x
+    for i in range(2, n+1):
+        series = series + (x ** i) / i
+    pprint(series, order='rev-lex')
+
+
+if __name__ == '__main__':
+    n = input('Enter the number of terms you want in the series: ')
+    print_series(int(n))
